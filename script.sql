80bf2feeeac6e1802f3c936a4f1f632f078613a4
Debora <ds336756@gmail.com>
2025-05-08 15:24:47 -0300

    Adicionando o script do Banco de Dados

---
+++ script.sql
@@ -0,0 +1,20 @@
+create database dbServidor;
+use dbServidor;
+
+create table Usuario (
+Id int primary key,
+Nome varchar(50) not null,
+Email varchar(100) not null,
+Senha varchar(50) not null
+);
+
+create table Produto (
+Id int primary key,
+Nome varchar(50) not null,
+Descricao varchar(100) not null,
+Preco decimal(5,2) not null,
+Quantidade int not null
+);
+
+select * from Usuario;
+select * from Produto;
