f3e6e281ae69e85c9967d616ad0f9774cb4f7aa9
Debora <ds336756@gmail.com>
2025-05-08 15:44:40 -0300

    Adicionando as models de Usuario e Produto

---
+++ GamesEcommerce/Models/Produto.cs
@@ -0,0 +1,11 @@
+﻿namespace GamesEcommerce.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Descricao { get; set; }
+        public decimal? Preco { get; set; }
+        public int Quantidade { get; set; }
+    }
+}
