2929e766488e452a9d26367326a74fcc5fd231bd
Debora <ds336756@gmail.com>
2025-05-08 15:09:56 -0300

    Criando Projeto

---
+++ GamesEcommerce/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GamesEcommerce.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
