851276dcdef27b40ad0b7b76c39be74d82b2b2b2
Debora <ds336756@gmail.com>
2025-05-13 01:39:21 -0300

    Adicionando o código de ProdutoRepositorio e LoginRepositorio

--- GamesEcommerce/Repositorio/LoginRepositorio.cs
+++ GamesEcommerce/Repositorio/LoginRepositorio.cs
@@ -1,9 +1,40 @@
-﻿using MySql.Data.MySqlClient;
+﻿using GamesEcommerce.Models;
+using MySql.Data.MySqlClient;
 using System.Data;
 
 namespace GamesEcommerce.Repositorio
 {
-    public class LoginRepositorio
+    public class LoginRepositorio(IConfiguration configuration)
     {
+        private readonly string _conexaoMySQL = configuration.GetConnectionString("conexaoMySQL");
+
+        public Usuario ObterLogin(string email)
+        {
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+
+                MySqlCommand cmd = new("select * from Usuario where Email = @email", conexao);
+                cmd.Parameters.Add("@email", MySqlDbType.VarChar).Value = email;
+
+                using (MySqlDataReader dr = cmd.ExecuteReader(CommandBehavior.CloseConnection))
+                {
+                    Usuario usuario = null;
+                    
+                    if (dr.Read())
+                    {
+                        usuario = new Usuario
+                        {
+                            Id = Convert.ToInt32(dr["Id"]),
+                            Nome = dr["Nome"].ToString(),
+                            Email = dr["Email"].ToString(),
+                            Senha = dr["Senha"].ToString()
+                        };
+                    }
+
+                    return usuario;
+                }
+            }
+        }
     }
 }
