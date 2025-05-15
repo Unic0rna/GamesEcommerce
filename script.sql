1f846e90d04f6779e64c051b67f844e0369ba986
Debora <ds336756@gmail.com>
2025-05-15 04:04:29 -0300

    Ajustando css

--- script.sql
+++ script.sql
@@ -2,14 +2,14 @@ create database dbServidor;
 use dbServidor;
 
 create table Usuario (
-Id int primary key,
+Id int primary key auto_increment,
 Nome varchar(50) not null,
 Email varchar(100) not null,
 Senha varchar(50) not null
 );
 
 create table Produto (
-Id int primary key,
+Id int primary key auto_increment,
 Nome varchar(50) not null,
 Descricao varchar(100) not null,
 Preco decimal(5,2) not null,
