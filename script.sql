35a66ed572f8a427bdc23ef70f0f7b33503e7fd5
Debora <ds336756@gmail.com>
2025-05-15 14:13:10 -0300

    Arrumando coisas

--- script.sql
+++ script.sql
@@ -15,6 +15,6 @@ Descricao varchar(100) not null,
 Preco decimal(5,2) not null,
 Quantidade int not null
 );
-
+insert into Usuario (Nome, Email, Senha) values ('Debora','deb@gmail.com',123);
 select * from Usuario;
 select * from Produto;
